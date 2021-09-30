e30fce7a2467b5f9748a298b04108b33b3056130
joao Castro <joao_lukas97@hotmail.com>
2021-09-29 23:32:31 -0300

    laco de repeticao no python

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,28 @@ print("Bem vindo ao jogo de adivinhacao!")
 print("*********************************")
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1;
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_tentativas):
+    print("tentativa", rodada, "de", total_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("voce digitou ", chute_str)
+    chute = int(chute_str)
 
-print("voce digitou ", chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-chute = int(chute_str)
+    if (acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu numero foi maior do que o numero secreto.")
+        elif(menor):
+            print("Você errou! O seu numero foi menor do que o numero secreto.")
 
-if (numero_secreto == chute):
-    print("Voce acertou!")
-else:
-    print("Voce errou!")
+    rodada = rodada + 1
+
+
+print("Fim do jogo")
